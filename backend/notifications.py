87bcf397519ba779a32ac9879de11623f3f9490c
Francis Bergin <bergin.francis@gmail.com>
2018-01-27 17:21:12 -0500

    Move database url into env file

--- backend/notifications.py
+++ backend/notifications.py
@@ -1,13 +1,15 @@
 """
 """
 
+import os
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
 
 cred = credentials.Certificate('service-account-key.json')
-app = firebase_admin.initialize_app(cred, {'databaseURL' : 'https://conuhacks2018-mear.firebaseio.com'})
+app = firebase_admin.initialize_app(cred, {'databaseURL' : os.environ['DATABASE_URL']})
 root = db.reference()
 
 
